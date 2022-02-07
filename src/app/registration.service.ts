03fc1694740dcfd0f8db4f2607d92c15fbbd3670
Zhdzd <zahidabdullah18@gmail.com>
2022-02-07 22:50:14 +0800

    commit

---
+++ src/app/registration.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { lastValueFrom } from "rxjs";
+import { Registration, ResponseMessage } from "./models";
+
+
+const URL_POST_API_REGISTER= 'http://localhost:8080/api/register';
+
+@Injectable()
+export class RegistrationService {
+
+    constructor(private http: HttpClient){}
+
+    postRegistration(reg: Registration): Promise<ResponseMessage>{
+          const headers = new HttpHeaders()
+              .set('content-Type', 'application/x-www-form-urlencoded')
+
+          const params = new HttpParams()
+              .set('name', reg.name)
+              .set('email', reg.email)
+
+          return lastValueFrom(
+            this.http.post<ResponseMessage>(URL_POST_API_REGISTER, reg, {headers})
+          )
+    }
+}
